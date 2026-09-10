e00f4231ac9efbccafa0e3bd5fa9151ebc0024ec
bigchui <146177721+Nowake-stack@users.noreply.github.com>
2026-09-10 18:12:54 +0800

    Add files via upload

---
+++ demo4/demo4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    // printf("abc\ndef");
+    // printf("are you ok\?\?");
+    // printf("ok",'\'');
+    printf("%s\n","qwrt");
+    printf("%s\n","c:\\test\\16");
+    printf("\a");
+    return 0;
+}
+
