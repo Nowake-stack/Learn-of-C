797c2285c106488569fe7b962960e64d4a03216e
bigchui <146177721+Nowake-stack@users.noreply.github.com>
2026-09-10 22:13:06 +0800

    Add files via upload

--- demo4/demo4.c
+++ demo4/demo4.c
@@ -3,9 +3,13 @@ int main() {
     // printf("abc\ndef");
     // printf("are you ok\?\?");
     // printf("ok",'\'');
-    printf("%s\n","qwrt");
-    printf("%s\n","c:\\test\\16");
-    printf("\a");
+    // printf("%s\n","qwrt");
+    // printf("%s\n","c:\\test\\16");
+    // printf("\a");   //警报警报
+    // printf("abcdef\b");  //退格
+    // printf("abcdef\n");  //换行
+    // printf("abcdef\r");  //回车
+    printf("abc\tdef\t4567\t21233\t2");  //水平制表符
     return 0;
 }
 
